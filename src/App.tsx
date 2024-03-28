752a75c37d87a8065e7bc6a50927ef4a3c4d6d2f
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2024-03-28 11:20:22 -0300

    feat: configures import aliases on the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
-import { MainStack } from './navigators';
+import { MainStack } from '@navigators';
 import './locales';
 
 const App: React.FC = () => {

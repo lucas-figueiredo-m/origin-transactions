5fbb4e9db0e25a29d23fe4a0f9e9396bd299a524
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2024-03-28 19:41:02 -0300

    feat: configures firebase for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { MainStack } from '@navigators';
 import '@locales';
 import { Provider } from 'react-redux';
 import { store } from '@store';
+import Crashlytics from '@react-native-firebase/crashlytics';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      Crashlytics().crash();
+    }, 5000);
+  }, []);
   return (
     <Provider store={store}>
       <NavigationContainer>

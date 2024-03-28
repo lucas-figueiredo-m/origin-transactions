28e85d8a24d66db7db29fd4dd9d57570d776dee8
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2024-03-28 20:33:24 -0300

    fix: removes crashlytics forced crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useEffect } from 'react';
+import React from 'react';
 import 'react-native-gesture-handler';
 import { MainStack } from '@navigators';
 import '@locales';
 import { Provider } from 'react-redux';
 import { store } from '@store';
-import Crashlytics from '@react-native-firebase/crashlytics';
 
 const App: React.FC = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      Crashlytics().crash();
-    }, 5000);
-  }, []);
   return (
     <Provider store={store}>
       <NavigationContainer>

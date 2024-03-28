ee54d253810525ef57a992f84534cf571351bc19
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2024-03-27 23:15:07 -0300

    test: adds react-native-gesture-handler jest setup

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -5,6 +5,7 @@
 import 'react-native';
 import React from 'react';
 import App from '../src/App';
+import 'react-native-gesture-handler/jestSetup';
 
 // Note: import explicitly to use the types shipped with jest.
 import { it } from '@jest/globals';

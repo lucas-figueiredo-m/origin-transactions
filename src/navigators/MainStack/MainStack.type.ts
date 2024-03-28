c34f3e3902ec76c5774f508775747987203dae04
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2024-03-28 19:53:38 -0300

    feat: creates navigators typings

--- src/navigators/MainStack/MainStack.type.ts
+++ src/navigators/MainStack/MainStack.type.ts
@@ -1,7 +1,16 @@
-import { NavigatorScreenParams } from '@react-navigation/native';
+import { NavigatorScreenParams, RouteProp } from '@react-navigation/native';
 import { MainStackRoutes } from './MainStack.enum';
 import { AuthStackParamList } from '../AuthStack';
+import { StackNavigationProp } from '@react-navigation/stack';
 
 export type MainStackParamList = {
   [MainStackRoutes.AuthStack]: NavigatorScreenParams<AuthStackParamList>;
 };
+
+export type MainStackRouteParams<T extends MainStackRoutes> = RouteProp<
+  MainStackParamList,
+  T
+>;
+
+export type MainStackNavigationParams<T extends MainStackRoutes> =
+  StackNavigationProp<MainStackParamList, T>;
